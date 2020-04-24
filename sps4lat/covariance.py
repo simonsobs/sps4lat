Updated tests, corrected KL

sps4lat/covariance.py
@@ -136,9 +136,10 @@ def kl_divergence(emp_cov, model_cov, domain_list):
     except AssertionError:
         sys.exit('Empirical and modelled covmat have been computed over a'
                  'different number of subdomains')
-
-    inv_emp_cov = np.linalg.inv(emp_cov)
     m = emp_cov.shape[1]
+    inv_emp_cov = np.linalg.inv(emp_cov)
+    # BB : maybe use np.linalg.solve(emp_cov,np.broadcast_to(np.identity(
+    # m),emp_cov.shape)) instead ?
     _, logdet = np.linalg.slogdet(
         np.einsum('lab,lbc->lac', inv_emp_cov, model_cov))
     kl = .5 * (np.einsum('lab,lba->l', inv_emp_cov, model_cov) - logdet - m)
@@ -147,4 +148,4 @@ def kl_divergence(emp_cov, model_cov, domain_list):
     # multipoles : weights = np.array([(d.lmax - d.lmin + 1) * (
     # d.lmax+d.lmin+1) for d in domain_list])
 
-    return weights * kl
+    return (weights * kl).sum()

sps4lat/utils.py
@@ -48,7 +48,7 @@ def get_alms(maps, beams=None, lmax=None):
     Parameters
     ----------
     maps : list
-        list containing the frequency maps that have have different nside
+        list containing the frequency maps that can have different nside
     beams : ndarray
         beams associated with each map (assumed gaussian)
     lmax : int
@@ -66,7 +66,7 @@ def get_alms(maps, beams=None, lmax=None):
         lmax = min(lmax_maps_list)
     else:
         try:
-            assert (all([ll > lmax for ll in lmax_maps_list]))
+            assert (all([lmax <= ll for ll in lmax_maps_list]))
         except AssertionError:
             sys.exit("Some maps do not have the neccesary resolution to "
                      "resolve lmax={:d}, consider using a smaller "
@@ -85,8 +85,10 @@ def get_alms(maps, beams=None, lmax=None):
 
 
 if __name__ == '__main__':
-    nside = 12
+    nside = 2
     npix = hp.nside2npix(nside)
     map_test = np.random.randn(npix)
     alm_test = get_alms(map_test, beams=np.array([1.]))
     alm_sorted = sort_alms_ell(alm_test)
+    idx = [np.argwhere(alm_test == alm) for alm in alm_sorted]
+    print(idx)

tests/test_utils.py
@@ -7,26 +7,29 @@ from sps4lat import utils
 class UtilsTest(unittest.TestCase):
 
     def setUp(self):
-        self.nside = 1
-        self.lmax = 3 * self.nside - 1
-        self.cl = np.random.randn(self.lmax + 1)
-        self.alms = hp.synalm(self.cl, new=True)
-        self.maps = hp.alm2map(self.alms, nside=self.nside, verbose=False)
-        self.alms_sorted = self.alms[[0, 1, 3, 2, 4, 5]]
+        self.alm_2_sort = np.random.randn(5, hp.Alm.getsize(lmax=5))
+        idx_sorted = [0, 1, 6, 2, 7, 11, 3, 8, 12, 15, 4, 9, 13, 16, 18, 5,
+                      10, 14, 17, 19, 20]
+        self.alm_sorted = self.alm_2_sort[:, idx_sorted]
+        nside_list = [16, 32, 64, 128]
+        self.maps = [np.random.randn(hp.nside2npix(nside=nside)) for nside in
+                     nside_list]
+        self.lmax = 3. * 16. - 1.
 
     def test_sort_alms_ells(self):
         """ Test function that sorts the alms by ell. """
-        alms_sorted = utils.sort_alms_ell(self.alms)
+        alms_sorted = utils.sort_alms_ell(self.alm_2_sort)
         self.assertIsInstance(alms_sorted, np.ndarray)
-        self.assertTrue(np.all(alms_sorted[0] == self.alms_sorted))
+        self.assertTrue(np.all(alms_sorted == self.alm_sorted))
 
     def test_get_alms(self):
         """ Test function that get alms from maps. """
-        alms = utils.get_alms(self.maps, lmax=self.lmax)
+        alms = utils.get_alms(self.maps)
         self.assertIsInstance(alms, np.ndarray)
-        # self.assertTrue(np.all(alms[0] - self.alms < 1e-2))
-        # (BB) : not satisfied with this test ... #
-
+        self.assertSetEqual(set([hp.Alm.getlmax(len(alm)) for alm in alms]),
+                            {self.lmax})
+        with self.assertRaises(SystemExit):
+            _ = utils.get_alms(self.maps, lmax=100)
 
 if __name__ == '__main__':
     unittest.main()