Refactor of preprocessing and utils. Added binned spectra and correct computation of KL divergence.

sps4lat/optimize.py
@@ -7,19 +7,19 @@ from functools import reduce
 import scipy.optimize as opt
 
 
-def optimizable_function(experiment, model, param_default):
+def optimizable_function(emp_cov, model, param_default):
     model.prepare_for_arrays(param_default)
-    emp_cov = experiment.empirical_covmat
-
     def f(theta):
         model_cov = model.eval_array(theta)
-        kl = kl_divergence(emp_cov, model_cov, experiment)
-        return np.abs(kl)
+        kl = kl_divergence(emp_cov, model_cov)
+        # print(theta)
+        print(kl)
+        return kl
 
     return f
 
 
-def kl_divergence(emp_cov, model_cov, experiment):
+def kl_divergence(emp_cov, model_cov):
     """
     Compute the KL divergence between the empirical covariance matrix and the
     modelled one
@@ -29,51 +29,37 @@ def kl_divergence(emp_cov, model_cov, experiment):
         empirical covmat, shape is : ``(freqs,freqs,ells)``
     model_cov :ndarray
         modelled covmat, shape is : ``(freqs,freqs,ells)``
-    experiment : Experiment
     Returns
     -------
     KL divergence : measure of the mismatch between the two matrices.
 
     """
-    lmin_bins = np.arange(experiment.lmin, experiment.lmax, experiment.bin_size)
-    lmax_bins = np.append(lmin_bins[1:], experiment.lmax)
     try:
-        assert (emp_cov.shape[-2] == model_cov.shape[-2])
+        assert(emp_cov.shape == model_cov.shape)
     except AssertionError:
-        sys.exit('Empirical and modelled covmat have been computed at '
-                 'different frequencies')
-
-    try:
-        assert (emp_cov.shape[-1] == model_cov.shape[-1] == len(lmin_bins))
-    except AssertionError:
-
-        sys.exit('Empirical and modelled covmat have been computed over a'
-                 'different number of subdomains')
+        print("Empirical covmat and modle ones don't have the same shape")
+        return None
     n_bins = emp_cov.shape[-1]
-    m = emp_cov.shape[1]
+    lmax = n_bins + 1
     k = np.zeros(n_bins)
     for i in range(n_bins):
         emp = emp_cov[..., i]
         mod = model_cov[..., i]
-        Z = np.linalg.solve(np.linalg.cholesky(emp).T,
-                            np.linalg.cholesky(mod).T).T
-        k[i] = np.dot(Z.flatten().T, Z.flatten()) - 2. * np.sum(
-            np.log(np.diag(Z))) - m
-    # inv_emp = np.linalg.solve(emp,
-    #                           np.broadcast_to(np.identity(m), emp.shape))
-    # _, logdet = np.linalg.slogdet(
-    #     np.einsum('lab,lbc->lac', inv_emp, mod))
-    # kl = .5 * (np.einsum('lab,lba->l', inv_emp, mod) - logdet - m)
-    weights = np.array(
-        [(lmax_bins[i] - lmin_bins[i] + 1) * (lmax_bins[i] + lmin_bins[i - 1] + 1) for i in
-         range(len(lmin_bins))])
+        m = emp.shape[1]
+        Z = np.linalg.solve(np.linalg.cholesky(emp), np.linalg.cholesky(mod))
+        k[i] = .5*(np.dot(Z.flatten(), Z.flatten().T) - 2. * np.sum(
+            np.log(np.diag(Z))) - m)
+    weights = 2.*np.linspace(2, lmax, lmax-1) + 1.
+        # weights = np.array([(lmax_bins[i] - lmin_bins[i] + 1) * (
+        #             lmax_bins[i] + lmin_bins[i - 1] + 1) for i in
+        #                     range(len(lmin_bins))])
     return (k.dot(weights)).sum()
 
 
-def optimiser_test(experiment, model, param_start):
-    f_optim = optimizable_function(experiment, model, param_start)
+def optimiser_test(emp_cov, model, param_start):
+    f_optim = optimizable_function(emp_cov, model, param_start)
     theta_start = model.kwargs2array(param_start)
-    res = opt.minimize(f_optim, x0=theta_start, tol=1e-5,
-                       options={'maxiter': 10000})
+    res = opt.minimize(f_optim, x0=theta_start,
+                       options={'maxiter': 1000})
     param_opt = model.array2kwargs(res.x)
     return param_opt

sps4lat/utils.py
@@ -76,67 +76,35 @@ def get_alms(maps, beams=None, lmax=None):
             bl = hp.gauss_beam(np.radians(fwhm / 60.0), lmax)
             for i_alm, i_bl in zip(alm, bl.T):
                 hp.almxfl(i_alm, 1.0 / i_bl, inplace=True)
-    return alms
+    return np.array(alms)
 
 
-def bin_spectrum(cls, lmin=0, lmax=2000, bin_size=20.):
+def bin_spectra(ell, spectra, bins):
     """
-    Bin a spectrum
+    Bin sepctra in bins
     Parameters
     ----------
-    cls : ndarray
-        spectrum to bin, last dimension has to be ``ells``, going from 0 to
-        LMAX.
-    lmin : int
-        lowest ell to include in the returned spectra.
-    lmax : int
-        largest ell to include in the returned spectra.
-    bin_size : int
-        number of ells in a bin. (all bins are equal for now
+    ell: ndarray
+        ells for which spectra have been computed
+    spectra : ndarray
+        spectra, last dimension has to be ells
+    bins : ndarray or int
+        if ndarray specifies the bins to use, else, specifies N_bins
     Returns
     -------
-    cls_binned : ndarray
-        binned spectrum, last dimension indexes the bins.
+    ell_means : ndarray
+        mean ell in the bin, shape is ``(N_bins)``
+    spectra_means : ndarray
+        mean spectra in the bin, shape is ``(..., N_bins)``
     """
-
-    LMAX = cls.shape[-1] - 1
-    if lmax > LMAX:
-        lmax = LMAX
-    lmin_bins = np.arange(lmin, lmax, bin_size)
-    lmax_bins = np.append(lmin_bins[1:], lmax)
-    cls_binned = np.zeros(cls.shape[:-1] + (len(lmin_bins),))
-    for i in range(len(lmin_bins)):
-        cls_binned[..., i] = cls[..., lmin_bins[i]:lmax_bins[i]].mean(
-            axis=-1)
-    return cls_binned
-
-def get_ell_mean(lmin=0, lmax=2000, bin_size=20.):
-    """
-    Get min ell in bin
-    Parameters
-    ----------
-    lmin : int
-        lowest ell to include in the returned spectra.
-    lmax : int
-        largest ell to include in the returned spectra.
-    bin_size : int
-        number of ells in a bin. (all bins are equal for now
-    Returns
-    -------
-    ell_mean : ndarray
-        mean ell in each bin
-    """
-
-    lmin_bins = np.arange(lmin, lmax, bin_size)
-    lmax_bins = np.append(lmin_bins[1:], lmax)
-    lmean = (lmin_bins+lmax_bins)*.5
-    return lmean
-
-if __name__ == '__main__':
-    nside = 2
-    npix = hp.nside2npix(nside)
-    map_test = np.random.randn(npix)
-    alm_test = get_alms(map_test, beams=np.array([1.]))
-    alm_sorted = sort_alms_ell(alm_test)
-    idx = [np.argwhere(alm_test == alm) for alm in alm_sorted]
-    print(idx)
+    lmin = int(ell[0])
+    lmax = int(ell[-1])
+    if isinstance(bins, int):  # bins parameter specifies the number of bins
+        bins = np.linspace(lmin, lmax, bins).astype('int')
+    digitized = np.digitize(ell, bins)
+    ell_means = np.array([ell[digitized == i].mean(axis=-1) for i in
+                          range(1, len(bins))]).astype('int')
+    spectra_means = np.array(
+        [spectra[..., digitized == i].mean(axis=-1) for i in
+         range(1, len(bins))])
+    return ell_means, spectra_means